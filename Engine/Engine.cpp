1e320619968de985487fb2ba8b179ef3865d9c64
İsmail Özsaygı <devozsaygi@gmail.com>
2024-11-18 21:37:00 +0300

    Added initial dummy source files (Not sure if builds are stable at this point)

---
+++ Engine/Engine.cpp
@@ -0,0 +1,6 @@
+// This is totally dummy file and will be removed later on.
+
+int ReturnExactlySame(const int x)
+{
+    return x;
+}
---
+++ Player/Player.cpp
@@ -0,0 +1,5 @@
+// This is totally dummy file and will be removed later on.
+
+void DoNothing(void)
+{
+}
