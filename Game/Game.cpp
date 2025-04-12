5c1bebcc63bc9f5879a82df787266a5cf90908cf
İsmail Özsaygı <devozsaygi@gmail.com>
2025-04-12 20:33:41 +0300

    Fixed forgotten compile error

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -4,6 +4,6 @@
 // Color can be changed to demonstrate hot reloading.
 void Game_OnEngineRenderScene(SDL_Renderer* renderer, SDL_FRect rect) {
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderFillRect(renderer, &rect;
+    SDL_RenderFillRect(renderer, &rect);
     // SDL_RenderDrawRect(renderer, &rect);
 }
