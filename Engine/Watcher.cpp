c4a8e285e1cf87eb3aab5a9ed586d8c9352f0551
İsmail Özsaygı <devozsaygi@gmail.com>
2025-05-06 23:16:42 +0300

    Ported 'Watcher' to macOS

--- Engine/Watcher.cpp
+++ Engine/Watcher.cpp
@@ -1,6 +1,12 @@
 #include "Watcher.h"
 #include <cstdio>
 
+#ifdef __APPLE__
+#include <sys/event.h>
+#include <sys/fcntl.h>
+#include <unistd.h>
+#endif // __APPLE__
+
 #ifdef SDL_HOT_RELOAD_WIN32
 #include <windows.h>
 #endif // SDL_HOT_RELOAD_WIN32
@@ -62,6 +68,67 @@ int Watcher_TryCreate(const struct watcher* watcher, struct game_code* gc) {
 
     CloseHandle(handle);
     delete[] lpcwstr;
+
+    return 0;
+#elif __APPLE__
+    const int openHandle = open(watcher->directory, O_EVTONLY);
+    if (openHandle < 0) {
+        printf("Failed to open directory for watcher, the directory was: %s\n", watcher->directory);
+        return -1;
+    }
+
+    const int eventQueue = kqueue();
+    if (eventQueue == -1) {
+        printf("Failed to create event queue to read directory changes!");
+        close(openHandle);
+        return -1;
+    }
+
+    struct kevent directoryChangeEvent {};
+    EV_SET(&directoryChangeEvent, openHandle, EVFILT_VNODE, EV_ADD | EV_ENABLE | EV_CLEAR, NOTE_WRITE, 0, nullptr);
+
+    printf("Watcher created and entering the observation loop at %s\n", watcher->directory);
+
+    auto lastCapturedSystemTime = std::chrono::steady_clock::now();
+
+    while (watcher->isRunning) {
+        struct kevent currentEvent {};
+        timespec timeout = {1, 0};
+
+        const int fetchedEvent = kevent(eventQueue, &directoryChangeEvent, 1, &currentEvent, 1, &timeout);
+        if (fetchedEvent == -1) {
+            printf("Event error occured, terminating watcher!");
+            break;
+        }
+
+        if (fetchedEvent > 0) {
+            if (currentEvent.fflags & NOTE_WRITE) {
+                constexpr int changeRegistrationInterval = 5;
+                auto currentSystemTime = std::chrono::steady_clock::now();
+                auto elapsedSeconds =
+                    std::chrono::duration_cast<std::chrono::seconds>(currentSystemTime - lastCapturedSystemTime);
+
+                if (elapsedSeconds.count() >= changeRegistrationInterval) {
+                    printf("File modification detected, executing assigned callback\n");
+                    lastCapturedSystemTime = currentSystemTime;
+
+                    try {
+                        watcher->callback(gc);
+                    } catch (const std::exception& ex) {
+                        printf("Exception occurred while executing callback: %s\n", ex.what());
+                    }
+                }
+            }
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    printf("Finished observation loop for watcher\n");
+
+    // Close file and event handles.
+    close(eventQueue);
+    close(openHandle);
 
     return 0;
 #else
--- Game/Game.cpp
+++ Game/Game.cpp
@@ -5,5 +5,5 @@
 void Game_OnEngineRenderScene(SDL_Renderer* renderer, SDL_FRect rect) {
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderFillRect(renderer, &rect);
-    // SDL_RenderDrawRect(renderer, &rect);
+    // SDL_RenderRect(renderer, &rect);
 }
