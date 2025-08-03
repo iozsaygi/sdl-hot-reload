131e500eef1b2c3d5ff3f688fb668d119c191bd8
İsmail Özsaygı <devozsaygi@gmail.com>
2025-08-03 19:41:41 +0300

    Reset project

--- Game/Game.cpp
+++
@@ -1,9 +0,0 @@
-#include "Game.h"
-
-// Rendering very basic rectangle at the given position.
-// Color can be changed to demonstrate hot reloading.
-void Game_OnEngineRenderScene(SDL_Renderer* renderer, SDL_FRect rect) {
-    SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
-    SDL_RenderFillRect(renderer, &rect) ;
-    // SDL_RenderRect(renderer, &rect);
-}
--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir build
-cd build
-
-cmake .. -DCMAKE_BUILD_TYPE=Release
-
-cmake --build .
