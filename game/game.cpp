4a3a412f8683f4d0ac0d1197aeb66b4fe6616c2f
İsmail Özsaygı <devozsaygi@gmail.com>
2025-08-04 21:47:18 +0300

    Added SDL calls to game project

--- game/game.cpp
+++ game/game.cpp
@@ -1,1 +1,6 @@
 #include "game.h"
+
+void Game_OnEngineRenderScene(SDL_Renderer* renderer, const SDL_FRect rect) {
+    SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+    SDL_RenderFillRect(renderer, &rect);
+}
--- game/game.h
+++ game/game.h
@@ -1,4 +1,8 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <SDL3/SDL.h>
+
+void Game_OnEngineRenderScene(SDL_Renderer* renderer, SDL_FRect rect);
+
 #endif // GAME_H
