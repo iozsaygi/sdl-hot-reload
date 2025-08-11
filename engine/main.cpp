44d7b2e198d88d867e4fcdc70022c2fc83c58177
İsmail Özsaygı <devozsaygi@gmail.com>
2025-08-11 10:23:29 +0300

    Updated engine entry code to support _WIN32 as well

--- engine/main.cpp
+++ engine/main.cpp
@@ -2,7 +2,14 @@
 
 int main(int argc, char* argv[]) {
     constexpr EngineWindow engineWindow = {640, 480, "SDL Hot Reload"};
-    constexpr GameCode gameCode = {false, "Game.dylib", nullptr, nullptr};
+
+    constexpr GameCode gameCode = {false,
+#ifdef _WIN32
+                                   "game.dll",
+#elif __APPLE__
+                                   "game.dylib",
+#endif // _WIN32
+                                   nullptr, nullptr};
 
     Engine engine(engineWindow, gameCode);
     engine.Update();
